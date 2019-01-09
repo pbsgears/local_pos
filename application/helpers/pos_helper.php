1fffe6ed245498a02180d1295a56bc06d8705a5d
Shafri <user>
2019-01-09 09:50:15 +0530

    SME-1343 removed outlet while updating the wifi password

--- application/helpers/pos_helper.php
+++ application/helpers/pos_helper.php
@@ -3646,11 +3646,10 @@ if (!function_exists('get_random_wifi_password')) {
 if (!function_exists('update_wifi_password')) {
     function update_wifi_password($id, $menuSalesID)
     {
-        /** We remove where house auto ID because it is already set while uploading wifi password in live. */
+        /** We remove wareHouseAutoID & id_store because it is already set while uploading wifi password in live. */
         $outletID = get_outletID();
         $data['menuSalesID'] = $menuSalesID;
         $data['isUsed'] = 1;
-        $data['id_store'] = $outletID;
         $data['is_sync'] = 0;
 
         $CI =& get_instance();

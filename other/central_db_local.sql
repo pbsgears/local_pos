e2e14a5b4c71aecc8cfce5b92d297e35144c5854
HassanSajadh <44675117+HassanSajadh@users.noreply.github.com>
2020-07-14 09:29:36 +0530

    Updated local POS dbs

--- other/central_db_local.sql
+++ other/central_db_local.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : central_db_local2
+Source Server         : LocalServer
+Source Server Version : 50622
+Source Host           : 192.168.52.5:3306
+Source Database       : central_db_local
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50622
 File Encoding         : 65001
 
-Date: 2018-11-30 12:37:07
+Date: 2020-07-14 09:26:02
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,6 +28,7 @@ CREATE TABLE `srp_erp_company` (
   `company_start_date` date NOT NULL,
   `company_url` varchar(100) DEFAULT NULL,
   `company_logo` varchar(255) DEFAULT NULL,
+  `company_secondary_logo` varchar(255) DEFAULT NULL,
   `company_default_currencyID` int(11) DEFAULT NULL,
   `company_default_currency` varchar(45) DEFAULT NULL,
   `company_default_decimal` tinyint(1) DEFAULT '2',
@@ -46,6 +47,7 @@ CREATE TABLE `srp_erp_company` (
   `company_city` varchar(100) DEFAULT NULL,
   `company_province` varchar(100) DEFAULT NULL,
   `company_postalcode` int(5) DEFAULT NULL,
+  `countryID` int(11) DEFAULT NULL,
   `company_country` varchar(100) DEFAULT NULL,
   `legalName` varchar(45) DEFAULT NULL,
   `textIdentificationNo` varchar(45) DEFAULT NULL,
@@ -63,11 +65,23 @@ CREATE TABLE `srp_erp_company` (
   `defaultTimezoneID` int(11) DEFAULT NULL,
   `confirmedYN` int(1) DEFAULT '0',
   `host` longtext,
+  `op_host` longtext,
   `db_username` longtext,
   `db_password` longtext,
   `db_name` longtext,
   `attachmentFolderName` varchar(255) DEFAULT NULL,
   `attachmentHost` varchar(255) DEFAULT NULL,
+  `subscriptionNo` varchar(100) DEFAULT NULL,
+  `registeredDate` date DEFAULT NULL COMMENT 'Date of company registation to the system',
+  `subscriptionStartDate` date DEFAULT NULL,
+  `subscriptionCurrency` int(11) DEFAULT NULL,
+  `subscriptionAmount` double DEFAULT NULL,
+  `implementationAmount` double DEFAULT NULL,
+  `paymentEnabled` int(1) DEFAULT '0',
+  `isInitialSubscriptionConfirmed` int(1) DEFAULT '0',
+  `isSubscriptionEnabled` int(1) DEFAULT '1',
+  `isSubscriptionDisabled` int(1) DEFAULT '0',
+  `adminType` int(1) DEFAULT '1',
   `createdUserGroup` varchar(11) DEFAULT NULL,
   `createdPCID` varchar(45) DEFAULT NULL,
   `createdUserID` varchar(45) DEFAULT NULL,
@@ -87,7 +101,7 @@ CREATE TABLE `srp_erp_company` (
 -- ----------------------------
 -- Records of srp_erp_company
 -- ----------------------------
-INSERT INTO `srp_erp_company` VALUES ('1', null, null, '', '', '0000-00-00', null, null, null, null, '2', null, null, '2', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '0', 'f8b5e3d46d48070ee6085c8340b70f742bc017a9ae6a17049a261ba600cd32d289984ac3c188bfd55f0a99f8dcd4bd272d6a6da4c7269e2aaff5bd26d7a78322WUCNKJ1QQ7oG4OukgndNOUuHZg2Wh07VMapbjOaSJaI=', 'dbfeb59dbdec2254b332d21204204d82cca9b1331fd0be84f3b34dc246c0c102a1009106e16a1f0b9ec10a7e36fd37db7994a3136f4ddaf534903cc684bbc7d5xjeX07LbOBlVZXpR58etHX/NxlLZ0yi3bCdGDt/hs7w=', '28331774853d5bb48af1c7b4e6dabd7bd2ca0aaa2f230795c3c78e1c0ee76aa0ee16246eef0775a7053b9a1e0c869fcba21bdfa83ef955cefc99561c2553e9ddDlGuE7Jh1lEPodxdoIOBDMECvAsz3ULNA4CVqVXCrGc=', '58488a5a585d26204a7e4bf3652048b9cb36a892bf9d865a6a67bb69176259d0862fe359914094fbd526e9e3a4e9cddae0289f04e9b69c57a77ced3624933219uuUav0ezjcEPnJTK1+iUgs7clyrGrqaxaSuOGwUPAe8=', null, null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `srp_erp_company` VALUES ('1', null, null, '', '', '0000-00-00', null, null, null, null, null, '2', null, null, '2', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, '0', 'f8b5e3d46d48070ee6085c8340b70f742bc017a9ae6a17049a261ba600cd32d289984ac3c188bfd55f0a99f8dcd4bd272d6a6da4c7269e2aaff5bd26d7a78322WUCNKJ1QQ7oG4OukgndNOUuHZg2Wh07VMapbjOaSJaI=', null, 'dbfeb59dbdec2254b332d21204204d82cca9b1331fd0be84f3b34dc246c0c102a1009106e16a1f0b9ec10a7e36fd37db7994a3136f4ddaf534903cc684bbc7d5xjeX07LbOBlVZXpR58etHX/NxlLZ0yi3bCdGDt/hs7w=', '28331774853d5bb48af1c7b4e6dabd7bd2ca0aaa2f230795c3c78e1c0ee76aa0ee16246eef0775a7053b9a1e0c869fcba21bdfa83ef955cefc99561c2553e9ddDlGuE7Jh1lEPodxdoIOBDMECvAsz3ULNA4CVqVXCrGc=', '58488a5a585d26204a7e4bf3652048b9cb36a892bf9d865a6a67bb69176259d0862fe359914094fbd526e9e3a4e9cddae0289f04e9b69c57a77ced3624933219uuUav0ezjcEPnJTK1+iUgs7clyrGrqaxaSuOGwUPAe8=', null, null, null, null, null, null, null, null, '0', '0', '1', '0', '1', null, null, null, null, null, null, null, null, null, null);
 
 -- ----------------------------
 -- Table structure for srp_erp_countrymaster
@@ -163,6 +177,8 @@ CREATE TABLE `user` (
   `companyID` int(11) DEFAULT NULL,
   `isSystemAdmin` tinyint(4) DEFAULT NULL,
   `randNum` longtext,
+  `login_token` varchar(500) DEFAULT NULL,
+  `groupID` int(11) DEFAULT NULL,
   `isGroupUser` int(1) DEFAULT '0',
   PRIMARY KEY (`EidNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
